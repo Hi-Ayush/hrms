25101f36f196e0defcc5d355c29ca20a1ad91c1e
Hi-Ayush <ayush.kumar.ee20@heritageit.edu.in>
2021-02-28 23:42:09 +0530

    ch

--- src/screen/login/Login.js
+++ src/screen/login/Login.js
@@ -1,8 +1,16 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import Button from '../../components/button/Button';
 import Card from '../../components/card/Card';
 import Input from '../../components/input/input';
 const Login=(props)=>{
+    const [email,setEmail]=useState('');
+    const [password,setPassword]=useState('');
+    const [errorMessage,setErrorMessage]=useState('');
+
+    useEffect(()=>{
+
+    })
+
 
     const loginClickHandler=()=>{
         const jwtToken="eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJzdWIiOiIxMjM0NTY3ODkwIiwibmFtZSI6IkpvaG4gRG9lIiwiaWF0IjoxNTE2MjM5MDIyfQ.SflKxwRJSMeKKF2QT4fwpMeJf36POk6yJV_adQssw5c";
@@ -10,13 +18,22 @@ const Login=(props)=>{
         // props.setLoggedIn(true);
         props.history.push('/');
     }
+      const emailChangeHandler=(event)=>{
+        setEmail(event.target.value);
+      }
 
+      const passwordChangeHandler=(event)=>{
+        setPassword(event.target.value);
+      }
+
+      console.log(email,password);
     return (
       <div className="login">
         <Card  dimension={{ width: 250, height: 350 }}>
           <div className="login-header"><h3>HRMS</h3></div>
-          <Input type="email" placeholder="Enter email" />
-          <Input type="password" placeholder="Enter password" />
+         {errorMessage && <p>{errorMessage }</p>}
+          <Input type="email" value={email} onChange={emailChangeHandler} placeholder="Enter email" />
+          <Input type="password" value={password} onChange={passwordChangeHandler} placeholder="Enter password" />
           <Button className="login-button">Login</Button>
         </Card>
       </div>
